d006a6f5b2c873184ccb64888a3a8beceb6a1725
sapirowsky <sapirowsky@gmail.com>
2022-02-14 19:26:27 +0100

    stopka

--- assets/js/partners.js
+++ assets/js/partners.js
@@ -1,6 +1,6 @@
 // menu partnerów
 const nexwell = document.querySelectorAll('#nexwell')
-const moeller = document.querySelectorAll('#moeller')
+const eaton = document.querySelectorAll('#eaton')
 const legrand = document.querySelectorAll('#legrand')
 const satel = document.querySelectorAll('#satel')
 const rightCol = document.querySelectorAll('#right-col')
@@ -10,9 +10,9 @@ nexwell[0].addEventListener('click', () => {
     addActive(nexwell)
 
 })
-moeller[0].addEventListener('click', () => {
+eaton[0].addEventListener('click', () => {
     hideActive()
-    addActive(moeller)
+    addActive(eaton)
 })
 legrand[0].addEventListener('click', () => {
     hideActive()
